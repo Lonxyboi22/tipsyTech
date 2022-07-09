018ab82759ca0ea9daaa64349e5d1a7540f0ec3c
Lonxyboi <drewthemachine22@gmail.com>
2022-07-09 17:55:35 -0400

    getting an error because there is no ./controllers

--- models/User.js
+++ models/User.js
@@ -1,5 +1,6 @@
 const { Model, DataTypes } = require('sequelize');
 const sequelize = require('../config/connection');
+const bcrypt = require('bcrypt');
 
 // create our User model
 class User extends Model {}
@@ -34,6 +35,13 @@ User.init(
     }
   },
   {
+    hooks: {
+  // set up beforeCreate lifecycle "hook" functionality
+    async beforeCreate(newUserData) {
+        newUserData.password = await bcrypt.hash(newUserData.password, 10);
+        return newUserData;
+     },
+    },
     sequelize,
     timestamps: false,
     freezeTableName: true,
