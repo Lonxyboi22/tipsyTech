1fbe7f87f7940eb9ee686eef293a6a8b1e3876df
Lonxyboi <drewthemachine22@gmail.com>
2022-07-13 18:47:56 -0400

    signup workinggit add -A!

--- public/javascript/signup.js
+++ public/javascript/signup.js
@@ -6,10 +6,9 @@ async function signupFormHandler(event) {
     const username = document.querySelector('#username-signup').value.trim();
     const email = document.querySelector('#email-signup').value.trim();
     const password = document.querySelector('#password-signup').value.trim();
-    const age = document.querySelector('#age-signup').value.trim();
 
-    if (username && email && password && age) {
-        const response = await fetch('/api/user-routes', {
+    if (username && email && password) {
+        const response = await fetch('/api/users', {
             method: 'post',
             body: JSON.stringify({
             username,
