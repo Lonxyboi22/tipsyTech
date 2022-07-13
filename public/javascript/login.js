c42bfe80de02d3ed1cac398301a5baff5578369f
Lonxyboi <drewthemachine22@gmail.com>
2022-07-13 11:29:58 -0400

    fixing signup

--- public/javascript/login.js
+++ public/javascript/login.js
@@ -50,4 +50,3 @@ async function loginFormHandler(event) {
 
 document.querySelector('.login-form').addEventListener('submit', loginFormHandler);
 
-document.querySelector('.signup-form').addEventListener('submit', signupFormHandler);
