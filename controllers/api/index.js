a7a921023f214204d8dcb08846702f1738bcbec3
Jason L <jkleitz27@gmail.com>
2022-07-15 19:31:28 -0400

    updated and added routing for beverages

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -4,8 +4,23 @@ const userRoutes = require('./user-routes.js');
 const postRoutes = require('./post-routes');
 const commentRoutes = require('./comment-routes');
 
+const bourbonRoutes = require('./bourbon-routes');
+
+const tequilaRoutes = require('./tequila-routes');
+
+const rumRoutes = require('./rum-routes');
+const whiskeyRoutes = require('./whiskey-routes');
+const wineRoutes = require('./wine-routes');
+
+
 router.use('/users', userRoutes);
 router.use('/posts', postRoutes);
 router.use('/comments', commentRoutes);
+router.use('/bourbon', bourbonRoutes);
+router.use('/tequila', tequilaRoutes);
+router.use('/rum', rumRoutes);
+router.use('/whiskey', whiskeyRoutes);
+router.use('/wine', wineRoutes);
+
 
 module.exports = router;
---
+++ controllers/api/rum-routes.js
@@ -0,0 +1,86 @@
+const router = require('express').Router();
+const sequelize = require('../../config/connection');
+const { Rum, User } = require('../../models');
+const withAuth = require('../../utils/auth');
+
+// get all users
+router.get('/', (req, res) => {
+    console.log('======================');
+    Rum.findAll({
+        attributes: [
+        'id',
+        'name',
+        'ingredients',
+        'image'
+        ],
+    })
+    .then(dbRumData => res.json(dbRumData))
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
+});
+
+router.get('/:id', (req, res) => {
+    Rum.findOne({
+        where: {
+        id: req.params.id
+        },
+        attributes: [
+            'id',
+            'name',
+            'ingredients',
+            'image'
+        ],
+    })
+    .then(dbRumData => {
+        if (!dbRumData) {
+            res.status(404).json({ message: 'No post found with this id' });
+            return;
+        }
+        res.json(dbRumData);
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
+});
+
+router.post('/', (req, res) => {
+
+    Rum.create({
+        name: req.body.name,
+        ingredients: req.body.ingredients,
+    })
+        .then(dbRumData => res.json(dbRumData))
+        .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
+});
+
+
+
+
+router.delete('/:id', withAuth, (req, res) => {
+    Rum.destroy({
+        where: {
+        id: req.params.id
+        }
+    })
+    .then(dbRumData => {
+        if (!dbRumData) {
+            res.status(404).json({ message: 'No post found with this id' });
+            return;
+        }
+        res.json(dbRumData);
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
+});
+
+
+
+module.exports = router;
