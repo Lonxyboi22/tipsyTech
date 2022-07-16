2a0686af489a85b227671616b2e39a09e23cf0f0
Lonxyboi <drewthemachine22@gmail.com>
2022-07-16 11:00:02 -0400

    merged

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -5,19 +5,10 @@ const postRoutes = require('./post-routes');
 const commentRoutes = require('./comment-routes');
 
 const bourbonRoutes = require('./bourbon-routes');
-<<<<<<< HEAD
-
-const tequilaRoutes = require('./tequila-routes');
-
-const rumRoutes = require('./rum-routes');
-const whiskeyRoutes = require('./whiskey-routes');
-const wineRoutes = require('./wine-routes');
-=======
 const tequilaRoutes = require('./tequila-routes');
 const rumRoutes = require('./rum-routes');
 const whiskeyRoutes = require('./whiskey-routes');
 const vodkaRoutes = require('./vodka-routes');
->>>>>>> feature/seeding-drinks
 
 
 router.use('/users', userRoutes);
@@ -27,12 +18,7 @@ router.use('/bourbon', bourbonRoutes);
 router.use('/tequila', tequilaRoutes);
 router.use('/rum', rumRoutes);
 router.use('/whiskey', whiskeyRoutes);
-<<<<<<< HEAD
-router.use('/wine', wineRoutes);
-
-=======
 router.use('/vodka', vodkaRoutes);
->>>>>>> feature/seeding-drinks
 
 
 module.exports = router;
